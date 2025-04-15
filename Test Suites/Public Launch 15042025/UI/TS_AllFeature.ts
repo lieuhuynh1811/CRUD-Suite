394e158f7204f0e47971c61af6ec24a145f7d241
lieu.huynh+core@katalon.com <katalon_g3_dummy_username>
2025-04-15 03:41:42 +0000

    Create "TS_AllFeature" test suite

---
+++ Test Suites/Public Launch 15042025/UI/TS_AllFeature.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>TS_AllFeature</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>0</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>false</rerunImmediately>
+    <testSuiteGuid>5395b483-d70a-476c-902f-301a5ddbe619</testSuiteGuid>
+    <testCaseLink>
+        <guid>e077f120-2828-43e5-a679-2505b27c2e19</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/Home Page</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId></variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>6cecda9c-817f-4486-8209-7906b5b75a86</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/Valid login into TO</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>14d98e7b-4ac8-4e28-8ce7-394001d188f0</variableId>
+        </variableLink>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>368fcd18-d894-44ce-812c-6fbd104be802</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/Valid logout into TO</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId></variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>eb94f45a-c74a-463f-a8bb-83fb3a2447d8</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Common Test Cases/Login</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
+        </variableLink>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
+        </variableLink>
+    </testCaseLink>
+</TestSuiteEntity>
+
