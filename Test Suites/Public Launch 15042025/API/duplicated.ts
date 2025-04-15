292ea7d03d0203924bed7fd8016cc921b5c50e8d
lieu.huynh+core@katalon.com <katalon_g3_dummy_username>
2025-04-15 09:10:54 +0000

    Create "duplicated" test suite

---
+++ Test Suites/Public Launch 15042025/API/duplicated.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>duplicated</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>0</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>false</rerunImmediately>
+    <testSuiteGuid>9255e718-c6b3-41fc-bcbf-fee226263446</testSuiteGuid>
+    <testCaseLink>
+        <guid>f03c665a-fe94-45ba-b1bd-db09935120e0</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/Valid logout into TO</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId></variableId>
+        </variableLink>
+    </testCaseLink>
+</TestSuiteEntity>
+
