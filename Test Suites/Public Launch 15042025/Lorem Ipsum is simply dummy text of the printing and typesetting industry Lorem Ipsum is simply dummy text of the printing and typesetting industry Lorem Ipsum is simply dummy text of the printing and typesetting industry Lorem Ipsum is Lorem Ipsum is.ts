699ba7e9feac6ebb088b06e1ef396443606d48b4
lieu.huynh+core@katalon.com <katalon_g3_dummy_username>
2025-04-15 10:10:05 +0000

    Create "Manual TCs" test folder

---
+++ Test Suites/Public Launch 15042025/Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is Lorem Ipsum is.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is Lorem Ipsum is</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>0</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>false</rerunImmediately>
+    <testSuiteGuid>e584be74-b008-4deb-865b-ab694f1f2ef3</testSuiteGuid>
+    <testCaseLink>
+        <guid>d19b518a-55df-421b-b504-c9ea48be3a3e</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/Valid login into TO</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>14d98e7b-4ac8-4e28-8ce7-394001d188f0</variableId>
+        </variableLink>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>0eacb83c-9c17-461a-ad5a-1b26f97f9962</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/Home Page</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId></variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>d3c1e3df-ee8c-4029-9c7a-ea3b3606159d</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/Valid logout into TO</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId></variableId>
+        </variableLink>
+    </testCaseLink>
+</TestSuiteEntity>
+
